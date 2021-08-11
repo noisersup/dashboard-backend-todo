e9d2b11dbe7168e3dd880c6aa7b201d1b1b6a34f
noisersup <patekiapl@gmail.com>
2021-08-11 22:19:13 +0200

    :construction: Stop crashing if mongodb is not up

--- main.go
+++ main.go
@@ -14,31 +14,41 @@ import (
 	han "kwiatek.xyz/todo-backend/handlers"
 )
 
-type DbConfig struct{
-	Address		string 
-	Port		int    
+type DbConfig struct {
+	Address string
+	Port    int
 }
 
-func main(){	
-	corsPtr := flag.Bool("cors",false,"Enable CORS mode for locally debugging purposes.")
+func main() {
+	corsPtr := flag.Bool("cors", false, "Enable CORS mode for locally debugging purposes.")
 	flag.Parse()
-	
-	config := getVars() 
 
-	log.Printf("Connecting to database on %s:%d",config.Address,config.Port)
-	db,err := database.ConnectToDatabase(config.Address+":"+fmt.Sprint(config.Port),"tasks","tasks")
-	if err != nil { log.Panic(err) }
+	config := getVars()
 
-	defer func(){
-		if err = db.Disconnect(); err!=nil{
-			log.Fatalf("Problem with disconnecting: %s",err.Error())
+	var db *database.Database
+
+	for {
+		log.Printf("Connecting to database on %s:%d", config.Address, config.Port)
+		d, err := database.ConnectToDatabase(config.Address+":"+fmt.Sprint(config.Port), "tasks", "tasks")
+		if err == nil {
+			db = d
+			break
+		}
+		log.Print(err)
+	}
+
+	defer func() {
+		if err := db.Disconnect(); err != nil {
+			log.Fatalf("Problem with disconnecting: %s", err.Error())
 		}
 	}()
 
-	tasks,err := db.GetTasks()
-	if err != nil { log.Panic(err) }
+	tasks, err := db.GetTasks()
+	if err != nil {
+		log.Panic(err)
+	}
 
-	for _,task := range tasks {
+	for _, task := range tasks {
 		log.Print(task)
 	}
 
@@ -48,42 +58,42 @@ func main(){
 
 	r.HandleFunc("/tasks", h.GetTasks).Methods("GET")
 	r.HandleFunc("/tasks/due", h.GetDues).Methods("GET")
-	
+
 	r.HandleFunc("/tasks", h.AddTask).Methods("POST")
 	r.HandleFunc("/tasks/{id}", h.RemoveTask).Methods("DELETE")
 	r.HandleFunc("/tasks/{id}", h.DoneTask).Methods("PATCH")
-	
-	var httpHandler http.Handler 
+
+	var httpHandler http.Handler
 
 	if *corsPtr {
 		log.Printf("Warning!!! You are running CORS enabled mode. Do not use it on production")
-		headersOk := handlers.AllowedHeaders([]string{"*","Content-Type"})
+		headersOk := handlers.AllowedHeaders([]string{"*", "Content-Type"})
 		originsOk := handlers.AllowedOrigins([]string{"*"})
 		methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"})
 		httpHandler = handlers.CORS(originsOk, headersOk, methodsOk)(r)
-	}else{
+	} else {
 		httpHandler = r
 	}
 
 	log.Printf("Starting http server on port :8000...")
-	log.Fatal(http.ListenAndServe(":8000",httpHandler))
+	log.Fatal(http.ListenAndServe(":8000", httpHandler))
 }
 
-func getVars() *DbConfig {	
+func getVars() *DbConfig {
 	var config DbConfig
 
 	config.Address = os.Getenv("DB_ADDRESS")
-	config.Port,_ = strconv.Atoi(os.Getenv("DB_PORT")) //default: 27017
+	config.Port, _ = strconv.Atoi(os.Getenv("DB_PORT")) //default: 27017
 
-	if config.Address=="" { 
+	if config.Address == "" {
 		log.Fatal("ENV variables did not set")
 	}
 
-	config.Address = "mongodb://"+config.Address
+	config.Address = "mongodb://" + config.Address
 
 	if config.Port == 0 {
 		log.Print("Port invalid or not provided. Setting default (27017)")
-		config.Port=27017
+		config.Port = 27017
 	}
 	return &config
 }
